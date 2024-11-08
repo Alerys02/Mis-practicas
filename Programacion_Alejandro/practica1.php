d04d7850885fdcec0159a8a704c4ef125cd40c41
Alerys02 <aleyadir02@gmail.com>
2024-11-07 19:40:10 -0500

    Add files via upload

---
+++ Programacion_Alejandro/practica1.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+
+<?php
+ECHO "Hola! Este es mi primer script en PHP<BR>	"; 
+//Esto es una linea de comentario
+
+#Esto también es una linea de comentario 
+
+/* Esto es un
+bloque de mas de una linea 
+de comentario*/
+
+$vartext="Hola! Ahora estoy usando variables para guardar texto<br>";
+echo $vartext;
+$varnum1=5;
+$varnum2=6;
+$res=$varnum1+$varnum2;
+echo "El resultado de la suma de $varnum1 y $varnum2 es = $res"; 
+?>
+
+</body>
+</html>
+
