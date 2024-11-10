4378ba8c86e738aef548cee872877183b90a9809
Alerys02 <aleyadir02@gmail.com>
2024-11-10 02:49:54 -0500

    Add files via upload

---
+++ php1.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+ <title>Mi primer Script en php </title>
+</head>
+<body>
+
+<?php
+ECHO "Hola! este es mi primer script en PHP  <BR>";
+
+//Esto es una linea de comentario
+
+#Esto también es una linea de comentario
+
+/*Esto es un
+bloque de más de una linea de comentarios*/
+
+$vartex="Hola! estoy usando vaiables para guardar texto <BR>"; 
+echo $vartex;
+$varnum1=5;
+$varnum2=6;
+$res=$varnum1+$varnum2;
+echo "El resultado de la suma de $varnum1 y varnum2 es =$res <BR>"; 
+?>
+</body>
+</html>
